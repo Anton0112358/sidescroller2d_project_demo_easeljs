6e061ba60cd643c3a81f5626c1f512fdf4180ebe
Anton Smith <zloyrimlinin@gmail.com>
2015-02-12 16:40:53 -0600

    just started

---
+++ main.js
@@ -0,0 +1,19 @@
+
+var game = (function namespace(){
+
+	var ns_model = (function namespace(){
+
+	})();
+
+	var ns_view = (function namespace(){
+	})();
+
+	var ns_controller = (function namespace(){
+	})();
+
+	return 
+	{
+
+	}
+
+})(); 
