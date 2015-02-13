26efea4e59ee80178f54b967b3b80af084a832b4
Anton Smith <zloyrimlinin@gmail.com>
2015-02-12 19:29:30 -0600

    added notes and code layout in main.js

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 
-var game = (function namespace(){
+var sidescroller_game = (function namespace(){
 
-	var ns_model = (function namespace(){
 
-	})();
+	// Models section: >>>
+	
+	var GameViewModel;
 
-	var ns_view = (function namespace(){
-	})();
+	var PlayerModel;
 
-	var ns_controller = (function namespace(){
-	})();
+	var EnemyModel;
 
+
+	// END Models section <<<
+
+	// Controllers section: >>>
+	
+	var GameController;
+
+	var PlayerController;
+
+	var EnemyController;
+
+	var KeyboardController;
+
+	// END Controllers section <<<
+	
+
+	// Game initiation section: >>>
+	
+	var load_game = function()
+	{
+		// Setting up events:
+		//
+			// ticker: on each tick call GameController.update_all();
+		//
+			// keyboard input event: on each keyboard event call KeyboardController.handle_keypress();
+		//
+			// on interrupt event: stop ticker
+
+		// Setting up other stuff:
+			// e.g setup canvas size
+	};
+	
 	return 
 	{
 
+		run: function(){
+			load_game();
+		};
 	}
 
 })(); 
